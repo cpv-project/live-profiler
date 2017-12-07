55d3ba5d55418fa46c7f986ea2436b9babdb2bcd
dudes-come <dudes@qq.com>
2017-12-07 10:17:22 +0800

    add TestLinuxProcessAddressLocator test

--- tests/Cases/Utils/Platform/Linux/TestLinuxProcessAddressLocator.cpp
+++ tests/Cases/Utils/Platform/Linux/TestLinuxProcessAddressLocator.cpp
@@ -1,13 +1,25 @@
+#include <unistd.h>
 #include <iostream>
 #include <cassert>
 #include <LiveProfiler/Utils/Platform/Linux/LinuxProcessAddressLocator.hpp>
 
 namespace LiveProfilerTests {
-	// using namespace LiveProfiler;
+	using namespace LiveProfiler;
+
+	void testLinuxProcessAddressLocatorLocateSelf() {
+		auto pathAllocator = std::make_shared<SingletonAllocator<std::string, std::string>>();
+		LinuxProcessAddressLocator locator;
+		locator.reset(::getpid(), pathAllocator);
+		auto address = reinterpret_cast<uintptr_t>(
+			&testLinuxProcessAddressLocatorLocateSelf) + 1;
+		auto pathAndOffset = locator.locate(address, false);
+		assert(pathAndOffset.first != nullptr);
+		assert(pathAndOffset.second > 0);
+	}
 
 	void testLinuxProcessAddressLocator() {
 		std::cout << __func__ << std::endl;
-		// TODO
+		testLinuxProcessAddressLocatorLocateSelf();
 	}
 }
 
