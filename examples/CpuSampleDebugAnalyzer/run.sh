e4b26f2ec6794880327c6a9bb783388cc8afe191
dudes-come <dudes@qq.com>
2017-12-11 17:53:37 +0800

    update run.sh in examples

--- examples/CpuSampleDebugAnalyzer/run.sh
+++ examples/CpuSampleDebugAnalyzer/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-g++ -Wall -Wextra --std=c++14 -I../../include Main.cpp -lbfd -o /tmp/a.out && /tmp/a.out $@
+g++ -Wall -Wextra --std=c++14 -O3 -g -I../../include Main.cpp -lbfd -o /tmp/a.out && /tmp/a.out $@
 
