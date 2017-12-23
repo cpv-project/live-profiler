3352973fbbc63dc799a6c169df2478705b7a62ab
dudes-come <dudes@qq.com>
2017-12-23 14:00:32 +0800

    update cmake (sucks)

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,8 +3,22 @@
 # ###################################################################
 set -e
 
+IS_WINDOWS="$(uname | grep MINGW || true)"
+IS_LINUX="$(uname | grep Linux || true)"
+
+if [ -n "${IS_WINDOWS}" ]; then
+	echo "Run tests on windows..."
+	SYSTEM_NAME="Windows"
+	export CXX=g++
+fi
+
+if [ -n "${IS_LINUX}" ]; then
+	echo "Run tests on linux..."
+	SYSTEM_NAME="Linux"
+fi
+
 mkdir -p Build
 cd Build
-cmake -DCMAKE_SYSTEM_NAME="Generic" ../
+cmake -DCMAKE_SYSTEM_NAME="${SYSTEM_NAME}" ../
 make
 ./LiveProfilerTest
