3410ca76fbf604025550cd3f21928028563aceac
dudes-come <dudes@qq.com>
2017-12-23 12:13:18 +0800

    fix cmake for windows

--- docs/Setup/Windows.md
+++ docs/Setup/Windows.md
@@ -1,7 +1,10 @@
-TODO
+# Environement Setup
+
 
 mingw64.exe
 
 pacman -Syu # update pacman itself
 pacman -Syu # update other packages
 pacman -S mingw-w64-x86_64-gcc cmake make
+
+https://sourceforge.net/projects/msys2
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # A test framework would be better but... I dont like bunch of macros
 # ###################################################################
+set -e
+
 mkdir -p Build
 cd Build
-cmake ../ && \
-make && \
+cmake -DCMAKE_SYSTEM_NAME="Generic" ../
+make
 ./LiveProfilerTest
-
